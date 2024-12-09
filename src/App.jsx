bfb8755e91a7e03085e756f3f78521b44bab129f
CodingPassToPass <codingiron24103@gmail.com>
2024-12-10 05:13:51 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
-// import PlayGame from "./component/playGame";
+import PlayGame from "./component/playGame";
+import NotFound from "./component/NotFound/NotFound";
 
 function App() {
   
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/editor/:id" element={<EditorPage/>} />
+        <Route path="/play" element={<play/>} />
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </>
   )
